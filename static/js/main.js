db6ca0830ef5a10c53f53c8ec4524da6bacd5eae
Kaoru <kaoru@gmail.com>
2024-06-29 11:03:22 +0800

    新增外部貼上main.css

---
+++ static/js/main.js
@@ -0,0 +1,61 @@
+
+// 呼叫函式
+drawPM25()
+
+// 動態繪製
+function drawPM25() {
+    $.ajax({
+        //找route資料
+        url: "/pm25-data",
+        type: "GET",
+        dataType: "json",
+        //如果前面都成功要做什麼? return出來的值
+        success: (result) => {
+            console.log(result);
+            drawChart(myChart, result["time"], "PM2.5", result["site"], result["pm25"])
+        }
+
+    }
+
+    )
+}
+const myChart = echarts.init(document.getElementById('main'));
+
+function drawChart(chart, title, legend, xData, yData,) {
+    // 基于准备好的dom，初始化echarts实例
+
+
+    // 指定图表的配置项和数据
+    let option = {
+        title: {
+            text: title
+        },
+        tooltip: {},
+        legend: {
+            data: [legend]
+        },
+        xAxis: {
+            data: xData
+        },
+        yAxis: {},
+        series: [
+            {
+                name: legend,
+                type: 'bar',
+                data: yData
+            }
+        ]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    chart.setOption(option);
+
+
+
+
+}
+
+
+
+
+
